4f294869ba9649e3910cda1774d8a2264f41417f
kalifun <kalifun@163.com>
2021-05-07 15:28:49 +0800

    :new: init

---
+++ src/extension.ts
@@ -0,0 +1,26 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "lazyswitch-vscode" is now active!');
+
+	
+	let Path = vscode.commands.registerCommand('lazyswitch-vscode.genyaml', (uri) => {
+		console.log("genyaml");
+		vscode.window.showInformationMessage(`当前文件夹路径是: ${uri ? uri.path : '空'}`);
+		vscode.workspace.openTextDocument(uri.path).then(document => {
+			console.log(document.getText);
+		});
+	});
+
+	context.subscriptions.push(Path);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
